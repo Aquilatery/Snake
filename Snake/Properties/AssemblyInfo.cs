51f65e52761ba4861a613c9e71c9d41a094eecf2
Taiizor <41683699+Taiizor@users.noreply.github.com>
2022-03-24 21:27:31 +0300

    .NET7 Support

--- Snake/Properties/AssemblyInfo.cs
+++ Snake/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Snake")]
-[assembly: AssemblyDescription("Snake v1.8")]
+[assembly: AssemblyDescription("Snake v1.9")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Taiizor")]
 [assembly: AssemblyProduct("Snake")]
-[assembly: AssemblyCopyright("Copyright © 2021 Taiizor")]
+[assembly: AssemblyCopyright("Copyright © 2022 Taiizor")]
 [assembly: AssemblyTrademark("Taiizor")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.0.0")]
-[assembly: AssemblyFileVersion("1.8.0.0")] 
+[assembly: AssemblyVersion("1.9.0.0")]
+[assembly: AssemblyFileVersion("1.9.0.0")] 
